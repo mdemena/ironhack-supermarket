85b83b5e9fa2fe2a74528ebbbd3c5d52d7f28c92
Marc De Mena <mdemena@gmail.com>
2020-05-21 21:23:34 +0200

    fixes

--- routes/carnesRouter.js
+++ routes/carnesRouter.js
@@ -1,53 +1,54 @@
-const express = require("express")
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
 
+const carnes = [
+	{
+		id: 1,
+		name: 'Filete de Ternera',
+		description: 'Filete de Ternera Origen Girona - 300gr',
+		price: 6,
+	},
+	{
+		id: 2,
+		name: 'Filetes de Pollo',
+		description: 'De granja ECO - 400gr',
+		price: 3.5,
+	},
+	{
+		id: 3,
+		name: 'Lomo de cerdo',
+		description: 'Lomo de cerdo de Vic - 250gr',
+		price: 3,
+	},
+	{
+		id: 4,
+		name: 'Chuletón de Buey',
+		description: 'Waygu Origen Galicia -ECO - 800gr',
+		price: 60,
+	},
+	{
+		id: 5,
+		name: 'Salchichas del país',
+		description: 'salchichas de cerdo - 6 unidades',
+		price: 2,
+	},
+];
 
-router.get("/", (req, res, next) => {
-    //buscar datos
-    //armar la pagina de resultados
-    console.log("carnes")
+router.get('/', (req, res, next) => {
+	//buscar datos
+	//armar la pagina de resultados
+	console.log('carnes');
 
-    const carnes = [{
-        id: 1 ,
-        name: "Filete de Ternera" ,
-        description: "Filete de Ternera Origen Girona - 300gr" ,
-        price: 6
-    },
-    {
-        id: 2,
-        name: "Filetes de Pollo" ,
-        description: "De granja ECO - 400gr" ,
-        price: 3.5 
-    },
-    {
-        id: 3,
-        name: "Lomo de cerdo" ,
-        description:"Lomo de cerdo de Vic - 250gr" ,
-        price: 3
-    },
-    {
-        id: 4 ,
-        name: "Chuletón de Buey",
-        description: "Waygu Origen Galicia -ECO - 800gr",
-        price: 60
-    },
-    {
-        id: 5,
-        name: "Salchichas del país",
-        description: "salchichas de cerdo - 6 unidades" ,
-        price: 2
-    }];
-
-
-    
-    res.render("carnes",{
-       carnes
-      })
-})
+	res.render('carnes', {
+		carnes,
+	});
+});
 
 router.get('/:id', (req, res, next) => {
 	if (req.params && req.params.id) {
-		res.render('carne', {carne: data.filter((el) => el.id === req.params.id) });
+		res.render('carne', {
+			carne: carnes.filter((el) => el.id === req.params.id),
+		});
 	}
 });
 
