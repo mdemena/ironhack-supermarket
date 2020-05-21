9095624e836a53f814740060a11489b97dccbc86
Marc De Mena <mdemena@gmail.com>
2020-05-21 21:01:22 +0200

    Beers

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ const hbs = require('hbs');
 const path = require('path');
 const chalk = require('chalk');
 const bodyparser = require('body-parser');
-
+const beersRouter = require('./routes/beersRouter');
 const app = express();
 
 app.use(bodyparser.urlencoded({ extended: true }));
+app.use('/beers', beersRouter);
 
-app.set('view engne', 'hbs');
+app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, '/views'));
 hbs.registerPartials(path.join(__dirname, '/views/partials'));
 
